2fb4567008666307da8cba32625e474da59ed212
ji <jiyongprk@gmail.com>
2022-01-13 23:13:20 -0500

    laying out the options design

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
+//creates header 
 const headerContainer = document.createElement("header");
 headerContainer.classList.add("header-container");
-console.log(headerContainer);
 document.body.appendChild(headerContainer);
-
+//creates title in h3
 const title = document.createElement("h1");
 title.setAttribute("id", "title")
 title.textContent="Etch-A-Sketch";
@@ -10,14 +10,47 @@ headerContainer.appendChild(title);
 
 
 
+
+//adds main container div
 const container = document.createElement("div");
 container.classList.add("grid-container");
-
-console.log(container);
 document.body.appendChild(container);
 
+//adds options div
+const optionsContainer = document.createElement("div");
+optionsContainer.classList.add("options-container");
+document.body.appendChild(optionsContainer);
 
 
+//things to do:
+
+//choose any color mode in options
+const color = document.createElement("button");
+color.setAttribute("id", "color");
+color.textContent="Color";
+optionsContainer.appendChild(color);
+//default color mode: black in option
+const defaultColor = document.createElement("button");
+defaultColor.setAttribute("id", "default-color");
+defaultColor.textContent="Default";
+optionsContainer.appendChild(defaultColor);
+//optional: rainbow mode
+const rainbow = document.createElement("button");
+rainbow.setAttribute("id", "rainbow");
+rainbow.textContent="Rainbow";
+optionsContainer.appendChild(rainbow);
+//eraser in options
+const eraser = document.createElement("button");
+eraser.setAttribute("id", "eraser");
+eraser.textContent="Eraser";
+optionsContainer.appendChild(eraser);
+//clear grid in options buttons format 
+const clear = document.createElement("button");
+clear.setAttribute("id", "clear");
+clear.textContent="Clear";
+optionsContainer.appendChild(clear);
+
+//ask for new number of squares in options slider/buttons format
 
 
 
@@ -69,5 +102,4 @@ setupGrid()
 const footer = document.createElement("footer");
 footer.setAttribute("id", "footer-container");
 footer.textContent="Copyright @NorfKorean";
-console.log(footer);
 document.body.appendChild(footer);
