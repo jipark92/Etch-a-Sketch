b642f1e0788ffb6b1c49ca056014c86530b35d5d
ji <jiyongprk@gmail.com>
2022-01-13 19:42:29 -0500

    add div within a div

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const container = document.createElement("div")
+const container = document.createElement("div");
 container.classList.add("container");
-
+container.textContent="hello world";
 document.body.appendChild(container);
 
-container.textContent="hello world";
+
+const gridBox = document.createElement("div");
+gridBox.classList.add("gridBox");
+gridBox.textContent="hello person";
+
+
+container.appendChild(gridBox);
