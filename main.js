ffd62f080a4221534057395a04d47f21ee461d9d
ji <jiyongprk@gmail.com>
2022-01-13 21:16:40 -0500

    add footer dom

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const container = document.createElement("div");
-container.classList.add("container");
-container.textContent="hello world";
+container.classList.add("grid-container");
+container.textContent="grid-container";
+console.log(container);
 document.body.appendChild(container);
-
 
 const gridBox = document.createElement("div");
 gridBox.classList.add("gridBox");
-gridBox.textContent="hello person";
-
-
+gridBox.textContent="gridbox";
 container.appendChild(gridBox);
+
+const footer = document.createElement("footer");
+footer.setAttribute("id", "footer-container");
+footer.textContent="Copyright @NorfKorean";
+console.log(footer);
+document.body.appendChild(footer);
